1427907fadc0e0c112527bb9359eea2d3cf55d05
mcmaceac <mcm.maceachern@gmail.com>
2017-11-10 17:06:24 -0500

    Added ImageAdapter from gridview and removed list view

--- Demo_Camera_mcmaceac/app/src/main/java/ca/yorku/eecs/mack/democameramcmaceac/ImageAdapter.java
+++ Demo_Camera_mcmaceac/app/src/main/java/ca/yorku/eecs/mack/democameramcmaceac/ImageAdapter.java
@@ -1,59 +1,73 @@
 package ca.yorku.eecs.mack.democameramcmaceac;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.io.File;
 
-class ImageAdapter extends BaseAdapter
+public class ImageAdapter extends BaseAdapter
 {
-    private final String[] fileNames;
-	private String directory;
-	private int displayWidth;
-	
-    private final ImageDownloader imageDownloader = new ImageDownloader();
-    
-    ImageAdapter(String[] fileNamesArg, String directoryArg, int displayWidthArg)
+    String[] filenames;
+    File directory;
+    ImageDownloader imageDownloader = new ImageDownloader();
+    int columnWidth;
+
+    private Context context;
+
+    public ImageAdapter(Context c)
     {
-    	fileNames = fileNamesArg;
-    	directory = directoryArg;
-    	displayWidth = displayWidthArg;
-    }
-   
-    @Override
-    public int getCount() 
-    {
-        return fileNames.length;
+        context = c;
     }
 
-    @Override
-    public String getItem(int position) 
+    public int getCount()
     {
-        return fileNames[position];
+        return filenames.length;
     }
 
-    @Override
-    public long getItemId(int position) 
+    public Object getItem(int position)
     {
-        return fileNames[position].hashCode();
+        return null;
     }
 
-    @Override
-    public View getView(int position, View view, ViewGroup parent) 
+    public long getItemId(int position)
     {
-        if (view == null) 
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        ImageView imageView;
+        if (convertView == null)
         {
-            view = new ImageView(parent.getContext());
-            view.setBackgroundColor(Color.LTGRAY);
-            ((ImageView)view).setScaleType(ImageView.ScaleType.CENTER);
-            view.setPadding(6, 6, 6, 6);
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(columnWidth, columnWidth));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        } else
+        {
+            imageView = (ImageView)convertView;
         }
 
-        String path = directory + File.separator + fileNames[position];
-        imageDownloader.download(path, (ImageView)view, displayWidth);       
-        return view;
+        // download the image and associate it with the ImageView
+        String path = directory + File.separator + filenames[position];
+        imageDownloader.download(path, imageView, 250);
+        return imageView;
+    }
+
+    // give the ImageAdapter the filenames array and the directory
+    public void setFilenames(String[] filenamesArg, File directoryArg)
+    {
+        filenames = filenamesArg;
+        directory = directoryArg;
+    }
+
+    public void setColumnWidth(int widthArg)
+    {
+        columnWidth = widthArg;
     }
 }
